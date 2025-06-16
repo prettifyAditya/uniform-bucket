16901476814546979f30220dddf947cf2fa82557
prettifyAditya <adityashukla.prettifycreative@gmail.com>
2025-06-16 12:44:02 +0530

    Added the resources to the project

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body cz-shortcut-listen="true">
         {children}
       </body>
     </html>
---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header(){
+    return(
+        <>
+            <header>
+                <div className="header-wrapper container">
+                    <div className="colA">
+                        
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
